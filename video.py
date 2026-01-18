f8677f462f8a5c6c04e3778fc230b1ce11550bc5
Martincoder-glitch <Martinsasuke20241@outlook.com>
2026-01-18 17:53:07 +0000

    Add files via upload

---
+++ video.py
@@ -0,0 +1,25 @@
+import cv2
+import os 
+from PIL import Image  
+mean_height=0
+mean_width=0
+
+num_of_images=len(os.listdir("."))
+
+for file in os.listdir("."):
+    img=Image.open(os.path.join(path,file))
+    width,height=img.size
+    mean_width=mean_width+width
+    mean_height=mean_height+height
+mean_width=mean_width//num_of_images
+mean_height=mean_height//num_of_images
+print(mean_width)
+print(mean_height)
+for file in os.listdir("."):
+    if file. endswidth(".jpg")or file.endswith(".jpeg")or file.endswith(".png"):
+        img=Image.open(os.path.join(path,file))
+        width,height=img.size
+        print(width,height)
+        imgresized=img.resize((mean_width,mean_height),Image.ANTIALIAS)
+        imgresized.save(file,"JPEG",quality=95)
+    
