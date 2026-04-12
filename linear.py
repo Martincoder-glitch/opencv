15b5d2ce7c077cf54e3ad34667b9deed8081f31f
Martincoder-glitch <Martinsasuke20241@outlook.com>
2026-04-12 17:57:33 +0100

    Add files via upload

---
+++ linear.py
@@ -0,0 +1,16 @@
+arr=[5,8,7,9,2,3]
+key=9
+
+if key in arr:
+    print("Key Exist")
+
+for i in range(0,len(arr)):
+    if arr[i] == key:
+        print("Key Exist")
+
+for num in arr:
+    if num == key:
+        print("Key Exist")
+
+
+
